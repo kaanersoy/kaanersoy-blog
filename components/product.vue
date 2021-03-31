a28a3d89aa60f502c22f296b1d161a0947cda80b
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-31 21:27:01 +0300

    Add product component for main page components

---
+++ components/product.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="project-box">
+    <div class="head">
+      <h5>{{ product.title }}</h5>
+    </div>
+    <div class="body">
+      <div class="content">
+        <nuxt-content :document="product" />
+      </div>
+      <div class="description">
+        <h6>Description:</h6>
+        <p>{{ product.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props:['product']
+}
+</script>
+
+<style lang="postcss">
+.project-box {
+  & + .project-box {
+    @apply border-t-2 border-gray-200 pt-6;
+  }
+  @apply my-6;
+  .head {
+    & > h5 {
+      @apply text-xl font-bold mb-4;
+    }
+  }
+  .body {
+    @apply xl:flex;
+    .content {
+      @apply xl:w-1/2 xl:mr-7;
+      img {
+        @apply w-full my-2;
+      }
+    }
+    .description {
+      h6 {
+        @apply border-b-2 mb-2 pb-2;
+      }
+      p {
+        @apply text-lg;
+      }
+    }
+  }
+}
+</style>
