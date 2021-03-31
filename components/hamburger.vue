80917d8231de16a46e5e12d3e7748f9452af081d
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-31 21:25:33 +0300

    Add hamburger menu file

---
+++ components/hamburger.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    @click="emit"
+    class="hamburger hamburger--elastic"
+    :class="isOpen ? 'is-active' : ''"
+    type="button"
+  >
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+</template>
+
+<script>
+import './hamburgers.css'
+
+export default {
+  name: 'Hamburger',
+  data: function(){
+    return{
+      isOpen: false,
+    }
+  },
+  methods:{
+    emit: function(){
+      this.isOpen = !this.isOpen;
+      this.$emit('menuToggle')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+button:focus {
+  outline: none;
+  border: none;
+}
+</style>
